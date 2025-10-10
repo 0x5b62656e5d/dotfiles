cd635fdbb4b6845d4c2d5bae1eda89d54ff38623
0x5b62656e5d <ben@benkou.dev>
2025-10-10 20:47:32 +0800

    Add install script for raspi zero2w for proxmark

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ My dotfiles with a pinch of peppers in it
 ## MacOS
 
 `curl -sSfL https://raw.githubusercontent.com/0x5b62656e5d/dotfiles/main/macos/install.sh | sh`
+
+## Proxmark Standalone Raspberry Pi Zero 2 W
+
+`curl -sSfL https://raw.githubusercontent.com/0x5b62656e5d/dotfiles/main/proxmarkpizero2w/install.sh | sh`
---
+++ proxmarkpizero2w/install.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+set -e
+
+echo "Prompting for sudo permissions..."
+sudo true
+
+# Initializing
+echo "Initializing installation script..."
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Installs git
+echo "Installing git..."
+sudo apt install git -y
+
+# Installs zsh
+echo "Installing zsh..."
+sudo apt install zsh -y
+
+# Installs oh-my-zsh
+echo "Installing oh-my-zsh..."
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Installs powerlevel10k theme
+echo "Installing powerlevel10k theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# Installs zsh-autosuggestions
+echo "Installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Installs zsh-syntax-highlighting
+echo "Installing zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Installs fastfetch
+echo "Installing fastfetch..."
+sudo apt install fastfetch -y
+
+# Installs ripgrep
+echo "Installing ripgrep..."
+sudo apt install ripgrep -y
+
+# Install nvim
+echo "Installing Neovim..."
+sudo apt install neovim -y
+
+# Installs nvm
+echo "Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# Installs nodejs
+echo "Installing Node.js..."
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+nvm use --lts
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+npm install -g pnpm
+
+# Install eza
+echo "Installing eza..."
+sudo apt install eza -y
+
+# install bat
+echo "Installing bat..."
+sudo apt install bat -y
+mkdir -p $HOME/.local/bin
+ln -s /usr/bin/batcat $HOME/.local/bin/bat
+
+# Install zoxide
+echo "Installing zoxide..."
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# Install btop
+echo "Installing btop..."
+sudo apt install btop -y
+
+# Install iputils-ping
+echo "Installing iputils-ping..."
+sudo apt install iputils-ping -y
+
+# Install net-tool
+echo "Installing net-tools..."
+sudo apt install net-tools -y
+
+# Install avahi-daemon for mDNS
+echo "Installing avahi-daemon..."
+sudo apt install -y avahi-daemon
+sudo systemctl enable --now avahi-daemon
+
+# Install toolchains
+echo "Installing toolchains..."
+sudo apt install -y ca-certificates gnupg unzip build-essential python3 python3-pip make gcc g++ pkg-config
+
+# Configuring dotfiles
+echo "Configuring dotfiles..."
+mkdir -p $HOME/.config/btop
+mkdir -p $HOME/.config/fastfetch
+rm -rf $HOME/.config/nvim
+
+git clone https://github.com/0x5b62656e5d/dotfiles.git $HOME/dotfiles
+cd $HOME/dotfiles
+cp $HOME/dotfiles/dots/ubuntuserver/.zshrc $HOME/.zshrc
+cp $HOME/dotfiles/dots/.p10k.zsh $HOME/.p10k.zsh
+cp $HOME/dotfiles/.config/btop/btop.conf $HOME/.config/btop/btop.conf
+cp $HOME/dotfiles/.config/fastfetch/config.jsonc $HOME/.config/fastfetch/config.jsonc
+cp -r $HOME/dotfiles/.config/nvim $HOME/.config/nvim
+
+echo "Installation complete!"
+echo "Rebooting system..."
+sudo reboot now
